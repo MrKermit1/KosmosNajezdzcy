e19176ae9364e60016a5e8d7599f997d3df14c5a
MrKermit <olafisz.pl@gmail.com>
2024-07-12 11:11:18 +0200

    added shooting

--- main.cpp
+++ main.cpp
@@ -5,25 +5,29 @@
 int main()
 {
 
-	const int SCREEN_HEIGTH = 720;
+	const int SCREEN_HEIGTH = 1000;
 	const int SCREEN_WIDTH = 1280;
 	//Enemy enemy = Enemy(100, 0, 60, 60, 3);
 	Enemy_grid grid;
 	InitWindow(SCREEN_WIDTH, SCREEN_HEIGTH, "Epicki Kosmiczny Pojedynek");
 
-	
+
 
 	SetTargetFPS(60);
 	while (!WindowShouldClose())
 	{
-		
-		BeginDrawing();
-		ClearBackground(BLACK);
 
+		BeginDrawing();
+		
+		ClearBackground(BLACK);
 		grid.Spawn();
 		grid.Move();
+		grid.Shoot();
+		
 
 		EndDrawing();
 	}
 	CloseWindow();
+
+	return 0;
 }
