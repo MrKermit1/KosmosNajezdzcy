72bc3f1295a76027f024f4d2ba2b551dc4a28062
MrKermit <olafisz.pl@gmail.com>
2024-07-12 11:09:38 +0200

    shooting is working

--- enemy.cpp
+++ enemy.cpp
@@ -1,28 +1,47 @@
 #include "enemy.h"
 #include "raylib.h"
+#include "bullet.h"
 
 Enemy::Enemy(){}
 
 Enemy::Enemy(int x, int y, int width, int heigth, int speed) : Player(x, y, width, heigth, speed)
 {
-	
+
 }
 
 void Enemy::Spawn()
 { 
 	DrawRectangleV(position ,size, WHITE);
+	if (shootOut)
+	{
+		bullet.Spawn();
+	}
+
 }
 
-
+Bullet Enemy::getBullet()
+{
+	return bullet;
+}
 
 void Enemy::Move()
 {
-	/*if (position.x >= 1280)
-	{
-		position.x = 0;
-		position.y += size.y;
-	}*/
 	position.x += speed;
+	bullet.Move();
 }
 
+void Enemy::Shoot()
+{	
+	if (!shootOut)
+	{
+		shootOut = true;
+		bullet = Bullet(position.x, position.y, 4);
+	}
 
+	if (bullet.getBulletPos().y > GetScreenHeight() - 100)
+	{
+		//bullet = Bullet(position.x, position.y, 4);
+		shootOut = false;
+	}
+
+}
--- enemy.h
+++ enemy.h
@@ -1,19 +1,22 @@
 #pragma once
 #include "player.h"
+#include "bullet.h"
 class Enemy: public Player
 {
 private:
 
-	void CheckBorderPos(int pos);
-
+	Bullet bullet;
+	bool shootOut = false;
 public:
 
-	Enemy();
 	Enemy(int x, int y, int width, int heigth, int speed);
+	Enemy();
 
 	void Move();
 	void Spawn();
 	void Shoot();
-	
+
+	Bullet getBullet();
+
 };
 
