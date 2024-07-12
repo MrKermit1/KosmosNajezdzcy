c12bd920fb178f599caf876c092284dee3ce6ee7
MrKermit <olafisz.pl@gmail.com>
2024-07-12 11:10:24 +0200

    movement will improve someday, shooting is working

--- enemy_grid.cpp
+++ enemy_grid.cpp
@@ -1,32 +1,25 @@
 #include "enemy_grid.h"
-
+#include <iostream>
 void Enemy_grid::Init()
 {
-
 	for (int i = 0; i < cols; i++)
 	{
 		for (int j = 0; j < rows; j++)
 		{
-			//maybe i should init those values idk
-			grid[i][j] = Enemy(i * 60 + 5, j * 60 + 5, 55, 55, 5);
+			isAlive[j][i] = true;
+			aliens[j][i] = Enemy(i * 60 + 5, j * 60 + 5, 55, 55, 3);
 		}
 	}
+	/*for (int i = 0; i < cols; i++)
+	{
+		grid[4][i] = true;
+	}*/
 }
 
 Enemy_grid::Enemy_grid()
 {
 	Init();
-}
-
-void Enemy_grid::Spawn()
-{
-	for (int i = 0; i < cols; i++)
-	{
-		for (int j = 0; j < rows; j++)
-		{
-			grid[i][j].Spawn();
-		}
-	}
+	lastShotTime = 0.0;
 }
 
 void Enemy_grid::Lower(bool border)
@@ -37,17 +30,34 @@ void Enemy_grid::Lower(bool border)
 		{
 			if (border)
 			{
-				grid[i][j].setPosX(1280 - (i * 60 + 1));
+				aliens[j][i].setPosX(1280 - (i * 60 + 1));
 			}
 			else
 			{
-				grid[i][j].setPosX(0 + (i * 60 + 1));
+				aliens[j][i].setPosX(0 + (i * 60 + 1));
 			}
-			grid[i][j].setSpeed(grid[i][j].getSpeed() * -1);
-			grid[i][j].setPosY(grid[i][j].getPosY() + grid[i][j].getHeigth());
-			
+			aliens[j][i].setSpeed(aliens[j][i].getSpeed() * -1);
+			aliens[j][i].setPosY(aliens[j][i].getPosY() + aliens[j][i].getHeigth());
+
 		}
 	}
+}
+
+void Enemy_grid::Shoot()
+{
+	double time = GetTime();
+	if (time - lastShotTime >= shootInterval)
+	{
+		int randomCol = GetRandomValue(0, cols - 1);
+		int randomRow = GetRandomValue(0, rows - 1);
+		if (isAlive[randomRow][randomCol])
+		{
+			aliens[randomRow][randomCol].Shoot();
+			lastShotTime = GetTime();
+		}
+	}
+	
+
 }
 
 void Enemy_grid::Move()
@@ -56,16 +66,32 @@ void Enemy_grid::Move()
 	{
 		for (int j = 0; j < rows; j++)
 		{
-			if (grid[i][j].getPosX() >= 1280)
+			if (aliens[j][i].getPosX() >= 1280)
 			{
 				Lower(true);
 			}
-			if (grid[i][j].getPosX() <= 0 - grid[i][j].getWidth())
+			if (aliens[j][i].getPosX() <= 0 - aliens[j][i].getWidth())
 			{
 				Lower(false);
 			}
-			grid[i][j].Move();
+			aliens[j][i].Move();
 		}
 	}
 }
 
+void Enemy_grid::Spawn()
+{
+
+	for (int i = 0; i < cols; i++)
+	{
+		for (int j = 0; j < rows; j++)
+		{
+			if (isAlive[j][i])
+			{
+				//DrawRectangle(i * 60 + 5, j * 60 + 5, 55, 55, WHITE);
+				aliens[j][i].Spawn();
+			}
+			
+		}
+	}
+}
--- enemy_grid.h
+++ enemy_grid.h
@@ -1,18 +1,22 @@
 #pragma once
 #include "enemy.h"
-class Enemy_grid : Enemy
+class Enemy_grid
 {
+private:
+	Bullet bullet;
+	int speed = 5;
+	const static int rows = 5;
+	const static int cols = 10;
+	bool isAlive[rows][cols];
+	float lastShotTime;
+	constexpr static float shootInterval = 0.50;
+	Enemy aliens[rows][cols];
 public:
-	static const int rows = 5;
-	static const int cols = 10;
-
-	Enemy grid[cols][rows];
-
+	void Init();
 	Enemy_grid();
 
-	void Init();
-	void Spawn();
 	void Lower(bool);
+	void Spawn();
 	void Move();
+	void Shoot();
 };
-
