cceda047014111e702c3b680da70afb3ef2ee2e2
MrKermit <olafisz.pl@gmail.com>
2024-07-23 18:57:29 +0200

    added game reset and player gui

--- main.cpp
+++ main.cpp
@@ -1,35 +1,74 @@
 #include "raylib.h"
-#include "Player.h"
+#include "player.h"
 #include "enemy_grid.h"
+#include <iostream>
+const int SCREEN_HEIGTH = 1000;
+const int SCREEN_WIDTH = 1280;
+Enemy_grid grid;
+Player player = Player(640, 900, 100, 50, 12);
+int playerLife = 3;
+bool gameOver = false;
+void gui()
+{
+	if (!gameOver)
+	{
+		DrawText(TextFormat("Score: %08i", player.getScore()), 50, 40, 20, WHITE);
+		DrawText(TextFormat("Life: %02i", player.getLife()), 1150, 40, 20, WHITE);
+	}
+	else {
+		DrawText(TextFormat("GAME OVER", player.getScore()), SCREEN_WIDTH/2, SCREEN_HEIGTH/2, 20, WHITE);
+	}
+	
+}
 
+void reset()
+{
+	if (player.getLife() + 1 == playerLife)
+	{
+		grid = Enemy_grid();
+		//Player player = Player(640, 900, 100, 50, 12);
+		player.setPosX(640);
+		player.setPosY(900.);
+		player.setWidth(100);
+		player.setHeigth(50);
+		
+		playerLife--;
 
+		if (player.getLife() == 0)
+		{
+			gameOver = true;
+		}
+	}
+	
+	
+}
 
 int main()
 {
 
-	const int SCREEN_HEIGTH = 1000;
-	const int SCREEN_WIDTH = 1280;
-	//Enemy enemy = Enemy(100, 0, 60, 60, 3);
-	Enemy_grid grid;
-	Player player = Player(640, 900, 100, 50, 12);
+
+
 	InitWindow(SCREEN_WIDTH, SCREEN_HEIGTH, "Epicki Kosmiczny Pojedynek");
 
 	SetTargetFPS(60);
 	while (!WindowShouldClose())
 	{
-
-
-		player.Move();
-		player.Shoot();
-		grid.CheckPlayerHits(player); 
-		grid.Move();
-		grid.Shoot();
-
-
 		BeginDrawing();
 		ClearBackground(BLACK);
-		grid.Spawn();
-		player.Spawn();
+		gui();
+		if (!gameOver)
+		{
+			player.Move();
+			player.Shoot();
+			grid.CheckPlayerHits(player);
+			grid.CheckAlienHits(player);
+			grid.Move();
+			grid.Shoot();
+			reset();
+			grid.Spawn();
+			player.Spawn();
+
+		}
 		EndDrawing();
 	}
 	CloseWindow();
