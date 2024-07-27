44a36d31bca235434d1bf5eed7b951cfb8c49e25
MrKermit <olafisz.pl@gmail.com>
2024-07-27 20:33:49 +0200

    added new feature to game (obstacles) and functions to control them

--- main.cpp
+++ main.cpp
@@ -1,11 +1,21 @@
 #include "raylib.h"
 #include "player.h"
 #include "enemy_grid.h"
+#include "obstacle.h"
 #include <iostream>
 const int SCREEN_HEIGTH = 1000;
 const int SCREEN_WIDTH = 1280;
+
 Enemy_grid grid;
 Player player = Player(640, 900, 100, 50, 12, 4);
+Obstacle obstacles[3] =
+{
+	Obstacle(955, 700),
+	Obstacle(500, 700),
+	Obstacle(10, 700)
+};
+
+
 int playerLife = player.getLife();
 bool gameOver = false;
 void GUI()
@@ -32,7 +42,11 @@ void Reset()
 		player.setWidth(100);
 		player.setHeigth(50);
 
-		
+		//this is so shitty
+		obstacles[0] = Obstacle(955, 700);
+		obstacles[1] = Obstacle(500, 700);
+		obstacles[2] = Obstacle(10, 700);
+		 
 		playerLife--;
 
 		if (player.getLife() == 0)
@@ -44,11 +58,41 @@ void Reset()
 	
 }
 
+void SpawnObstacles()
+{
+	for (int i = 0; i < 3; i++)
+	{
+		obstacles[i].Spawn();
+	}
+}
+
+void CheckPlayerWithObstacles(Obstacle *buff)
+{
+	if (buff != nullptr)
+	{
+		for (int i = 0; i < 3; i++)
+		{
+			auto& bullets = player.getBulltets();
+			for (auto it = bullets.begin(); it != bullets.end();)
+			{
+				if (it->isActive() && (buff + i)->CheckCollisionWithBullets(it))
+				{
+					it = bullets.erase(it);
+				}
+				else {
+					++it;
+				}
+			}
+		}
+	}
+	else {
+		std::cout << "null pointer detected!!!\n";
+	}
+	
+}
+
 int main()
 {
-
-
-
 	InitWindow(SCREEN_WIDTH, SCREEN_HEIGTH, "Epicki Kosmiczny Pojedynek");
 
 	SetTargetFPS(60);
@@ -62,11 +106,13 @@ int main()
 			player.Move();
 			player.Shoot();
 			grid.CheckPlayerHits(player);
-			grid.CheckAlienHits(player);
+			grid.CheckAlienHits(player, obstacles);
+			CheckPlayerWithObstacles(obstacles);
 			grid.Move();
 			grid.Shoot();
 			Reset();
 			grid.Spawn();
+			SpawnObstacles();
 			player.Spawn();
 
 		}
